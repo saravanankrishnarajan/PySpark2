acaf5ee242d03e6d2a477bb3af3371752fe5bf90
saravanankrishnarajan <32175950+saravanankrishnarajan@users.noreply.github.com>
2023-03-13 10:22:40 +0100

    Update memory_prof.py

--- memory_prof.py
+++ memory_prof.py
@@ -10,5 +10,13 @@ def example_ones():
     d = np.ones((100,1000,1000))
     return d
 
+@profile
+def example_two():
+    d = np.ones((100,1000,1000))
+    s = sum(d)
+    return s
+
 if __name__ == '__main__':
     example_ones()
+    example_two()
+
